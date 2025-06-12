a1469648e87e0cc3c9c880960ff3095bd02c4474
fizaan-ali <git config --global user.email>
2025-06-12 17:37:48 +0500

    auto-commit

--- arithmetic.c
+++ arithmetic.c
@@ -7,6 +7,25 @@ int main(){
     scanf("%d", &num2);
 
     // Addition
-    printf("Addition: %d + %d = %d", num1, num2, num1+num2);
+    printf("Addition: %d + %d = %d\n", num1, num2, num1+num2);
+
+    // Subtraction
+    printf("Subtraction: %d - %d = %d\n", num1, num2, num1-num2);
+
+    // Multiplication
+    printf("Multiplication: %d * %d = %d\n", num1, num2, num1*num2);
+
+    // Division
+    if(num2!=0){
+        printf("Integer Division: %d / %d = %d\n", num1, num2, num1/num2);
+        printf("Float Division: %d / %d = %.2f\n", num1, num2, (float)num1/num2 );
+    }
+    else printf("Error: Cannot performed by zero...");
+
+    // Modulus
+    if(num2!=0){
+        printf("Modulus: %d %% %d = %d\n", num1, num2, num1%num2);
+    }
+    else printf("Error: Cannot performed by zero..");
     return 0;
 }
