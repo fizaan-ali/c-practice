a81cd121affe8c393e20dbe09e303b834dcb8752
𝑭𝑰𝒁𝑨𝑨𝑵 <fizaan.mughal.01@gmail.com>
2025-06-13 16:15:38 +0500

    Auto updat

---
+++ numbers1ton.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int N;
+    printf("Enter a number: ");
+    scanf("%d", &N);
+    for(int i=1; i<=N; i++){
+        printf("%d ", i);
+    }
+
+    return 0;
+}
