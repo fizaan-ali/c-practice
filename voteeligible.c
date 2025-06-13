6d50b6197410925627c7f48c32d30da88a71e05a
fizaan-ali <git config --global user.email>
2025-06-13 15:56:58 +0500

    Auto update

--- voteeligible.c
+++ voteeligible.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
 int main(){
+    int age;
+    printf("Enter your age: ");
+    scanf("%d", &age);
+
+    if(age>18) printf("You're eligible to vote.");
+    else printf("You're not eligible to vote.");
     
     return 0;
 }
