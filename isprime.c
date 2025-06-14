0e235a8053dee59ebca815a0cc74fec09013c266
𝑭𝑰𝒁𝑨𝑨𝑵 <fizaan.mughal.01@gmail.com>
2025-06-14 13:31:32 +0500

    Auto update

---
+++ isprime.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+    int num, isPrime = 1;
+    printf("Enter the number: ");
+    scanf("%d", &num);
+
+    if(num == 0 || num == 1 || (num%2 == 0 && num != 2))
+         isPrime = 0;
+    if(num == 2)    isPrime = 1;
+    for(int i=3; i<=num/2; i+=2){
+        if(num % i == 0){    
+        isPrime = 0;
+        break;
+        }
+    }
+    if(isPrime == 0) printf("The given number is not prime.");
+    else printf("The given number is prime.");
+    
+    return 0;
+}
--- palindrome.c
+++ palindrome.c
@@ -3,17 +3,15 @@ int main(){
     int original, reversed = 0, digit;
     printf("Enter the number: ");
     scanf("%d", &original);
-
+    
     int num = original;
-    while(num!=0){
+    while(num != 0 ){
         digit = num % 10;
-        reversed = reversed *10 + digit;
+        reversed = reversed * 10 + digit;
         num /= 10;
     }
-    if(original == reversed){
-        printf("The given number is palindrome..");
-    }
-    else printf("The given number is not palindrome...");
-
+    if(original == reversed) printf("The given number is palindrome..");
+    else printf("The given number is not palindrome.");
+    
     return 0;
 }
