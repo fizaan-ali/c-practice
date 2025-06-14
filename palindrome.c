b1484ce2d3618b21183f88f50219af5ad88f9a21
𝑭𝑰𝒁𝑨𝑨𝑵 <fizaan.mughal.01@gmail.com>
2025-06-14 12:05:06 +0500

    Auto update

---
+++ palindrome.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+    int original, reversed = 0, digit;
+    printf("Enter the number: ");
+    scanf("%d", &original);
+
+    int num = original;
+    while(num!=0){
+        digit = num % 10;
+        reversed = reversed *10 + digit;
+        num /= 10;
+    }
+    if(original == reversed){
+        printf("The given number is palindrome..");
+    }
+    else printf("The given number is not palindrome...");
+
+    return 0;
+}
